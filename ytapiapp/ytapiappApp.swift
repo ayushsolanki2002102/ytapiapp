28a03c46ab81253c44a60b39750d5eb3ba4d3379
Ayush Solanki <ayushsolanki2002102@gmail.com>
2024-12-23 16:14:16 +0530

    Initial Commit

---
+++ ytapiapp/ytapiappApp.swift
@@ -0,0 +1,17 @@
+//
+//  ytapiappApp.swift
+//  ytapiapp
+//
+//  Created by Ayush Solanki on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ytapiappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ytapiappTests/ytapiappTests.swift
@@ -0,0 +1,17 @@
+//
+//  ytapiappTests.swift
+//  ytapiappTests
+//
+//  Created by Ayush Solanki on 23/12/24.
+//
+
+import Testing
+@testable import ytapiapp
+
+struct ytapiappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
